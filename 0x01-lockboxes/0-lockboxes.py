20849fb57db1324f23222657fd09699122c1ba55
sma3in96 <oumouss.ismail5@gmail.com>
2024-06-07 00:37:57 +0100

    type

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,24 @@
 Can unlock all the boxes.
 """
 
+
 def canUnlockAll(boxes):
-  """
-  Can unlock all the boxes.
-  """
-  if len(boxes) == 0:
-    return False
-  if len(boxes[0]) == 0:
-    return False
-  if len(boxes) == 1:
-    return True
-  if len(boxes[0]) == 1:
-    return True
-  for i in range(len(boxes)):
-    for j in range(len(boxes[0])):
-      if boxes[i][j] == 1:
-        for k in range(len(boxes[0])):
-          if boxes[i][k] == 1:
-            boxes[i][k] = 0
-  for i in range(len(boxes)):
-    for j in range(len(boxes[0])):
-      if boxes[i][j] == 1:
+    """
+    Can unlock all the boxes.
+    """
+    if not boxes:
         return False
-  return True
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
